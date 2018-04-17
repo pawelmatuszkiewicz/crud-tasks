8edaaad2d2f42b24987b31cbe01c1e11629f39ee
Pawel Matuszkiewicz <pmatuszk@gmail.com>
2018-04-17 21:26:21 +0200

    Add ex. 15.3

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -8,4 +8,13 @@ import java.util.List;
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
+
+    @Override
+    Task save(Task task);
+
+    @Override
+    Task findOne(Long id);
+
+    @Override
+    void delete(Long id);
 }
