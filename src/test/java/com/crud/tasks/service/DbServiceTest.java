76ac4dc96404bd3785f929fe8bc3330b8c56e265
Pawel Matuszkiewicz <pmatuszk@gmail.com>
2018-06-01 19:46:01 +0200

    Modify db service tests - Ex. 21.2

--- src/test/java/com/crud/tasks/service/DbServiceTest.java
+++ src/test/java/com/crud/tasks/service/DbServiceTest.java
@@ -1,16 +1,21 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import com.google.common.collect.Iterables;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,27 +23,31 @@ public class DbServiceTest {
     @Autowired
     private DbService dbService;
 
+    @MockBean
+    TaskRepository taskRepository;
+
     @Test
     public void shouldSaveTask() {
         // Given
-        Task task = new Task(100L,"dbService_test", "testing db service");
+        Task task = new Task(1L,"dbService_test", "testing db service");
+        Task savedTask = new Task(123L,"dbService_test", "testing db service");
+        when(taskRepository.save(task)).thenReturn(savedTask);
+        when(taskRepository.findOne(anyLong())).thenReturn(savedTask);
         // When
         Task returnedTask = dbService.saveTask(task);
         // Then
         long id = returnedTask.getId();
-        Optional<Task> savedTask = dbService.getTask(id);
-        assertEquals(id, savedTask.get().getId().longValue());
-
-        // Cleanup
-        dbService.deleteTask(id);
+        Optional<Task> readTask = dbService.getTask(id);
+        assertEquals(id, readTask.get().getId().longValue());
     }
 
     @Test
     public void shouldReturnNull() {
+        // Given
+        when(taskRepository.findOne(anyLong())).thenReturn(null);
         // When
-        List<Task> tasks = dbService.getAllTasks();
-        long id = tasks.get(tasks.size()-1).getId();
-        Optional<Task> readTask = dbService.getTask(id+1);
+        Optional<Task> readTask = dbService.getTask(100L);
+        System.out.println(readTask.orElse(null));
         // Then
         assertFalse(readTask.isPresent());
     }
